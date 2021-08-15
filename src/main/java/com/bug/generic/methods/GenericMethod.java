80dbd7e227500001f51ae49a808b571974b7f820
Krishan Kant Agarwal <agarwalkrishankant11@gmail.com>
2021-08-16 00:25:57 +0530

    Added Generics with Two Types

--- src/main/java/com/bug/generic/methods/GenericMethod.java
+++ src/main/java/com/bug/generic/methods/GenericMethod.java
@@ -1,8 +1,36 @@
 package com.bug.generic.methods;
 
+import java.util.Arrays;
+
+import com.bug.generic.model.Hashtable;
+
 public class GenericMethod {
 
 	public <T> void showItem(T item) {
 		System.out.println(item.toString());
+	}
+	
+	public <T> T showItem_ReturnItem(T item) {
+		return item;
+	}
+	
+	public <T, V> void showItem_WithTwoGenericType(T t, V v) {
+		System.out.println(t.toString());
+		System.out.println(v.toString());
+	}
+	
+	public <T, V> Hashtable<T, V> showItem_ReturnWithTwoGenericType(T t, V v) {
+		Hashtable<T, V> hashtable = new Hashtable<T, V>(t, v);
+		return hashtable;
+	}
+	
+	public <T> void showItems_UsingArray(T[] items) {
+		for (T t : items) {
+			System.out.println(t);
+		}
+	}
+
+	public <T> void showItems_UsingArrayToPrintSortedArray(T[] items) {
+		Arrays.stream(items).sorted().forEach(System.out::println);
 	}
 }
--- src/main/java/com/bug/generic/types/Generic_Three.java
+++ src/main/java/com/bug/generic/types/Generic_Three.java
@@ -1,6 +1,7 @@
 package com.bug.generic.types;
 
 import com.bug.generic.methods.GenericMethod;
+import com.bug.generic.model.Hashtable;
 import com.bug.generic.model.Store;
 
 public class Generic_Three {
@@ -15,6 +16,24 @@ public class Generic_Three {
 		method.showItem('G'); //char
 		method.showItem(true); //boolean
 		method.showItem(new Store<String>("From Store Class").toString()); //User defined Class
+		
+		//Returning value from Generic Method
+		String returnedItem = method.showItem_ReturnItem("Returning value from Generic Method");
+		System.out.println(returnedItem);
+		
+		//Using two Generic Types
+		method.showItem_WithTwoGenericType("I am one, and you are ..", 2);
+		
+		//Returning value from Generic Method having two types
+		Hashtable<String, Integer> table = method.showItem_ReturnWithTwoGenericType("Using HashTable for returning generic types" , 2);
+		System.out.println(table);
+		
+		//Using Array in Generic Method
+		method.showItems_UsingArray(new Integer[] {1,2,3,8,4});
+		
+		//Using Array in Generic Method and finding smaller number
+		System.out.println("**************** SORTED ARRAY ******************");
+		method.showItems_UsingArrayToPrintSortedArray(new Integer[] {91,5,69,2,14,5,6});
 	}
 
 }
